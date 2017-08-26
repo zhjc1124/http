9736d523629a6a7e5e9df8af47bbeba1ae407fbe
zhc1124 <qwerty762@163.com>
2017-08-26 22:12:55 +0800

    fix

--- manage.py
+++ manage.py
@@ -1,10 +1,13 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+if __name__ == '__main__':
+    from werkzeug.contrib.fixers import ProxyFix
+    app.wsgi_app = ProxyFix(app.wsgi_app)
+    app.run()
