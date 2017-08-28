350a06f53f81ea64f15c176c57a6c6b908715782
zhc1124 <qwerty762@163.com>
2017-08-28 17:32:27 +0800

    test post

--- manage.py
+++ manage.py
@@ -25,7 +25,8 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return 'sucess'
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
     return '''
     <!doctype html>
     <title>Upload new File</title>
