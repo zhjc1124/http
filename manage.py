6017f394c85d1edde01b711fbd777e38d2117edb
zhc1124 <qwerty762@163.com>
2017-08-28 15:52:46 +0800

    test post

--- manage.py
+++ manage.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'gif'])
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/image', methods=['POST'])
+def upload():
+    upload_file = request.files['image01']
+    if upload_file:
+        filename = secure_filename(upload_file.filename)
+        upload_file.save(os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], filename))
+        # file_content = request.files['image01'].stream.read()
+        return 'success'
+    else:
+        return 'failed'
 
 if __name__ == '__main__':
     from werkzeug.contrib.fixers import ProxyFix
