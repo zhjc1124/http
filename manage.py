246ea32a63055d2985f8f710c0ddd1028f8608cb
zhc1124 <qwerty762@163.com>
2017-08-28 16:48:03 +0800

    test post

--- manage.py
+++ manage.py
@@ -1,15 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'static/uploads/'
-app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'gif'])
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
 @app.route('/get')
@@ -17,16 +17,25 @@ def get():
     return 'test for get!'
 
 
-@app.route('/image', methods=['POST'])
-def upload():
-    upload_file = request.files['image01']
-    if upload_file:
-        filename = secure_filename(upload_file.filename)
-        upload_file.save(os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], filename))
-        # file_content = request.files['image01'].stream.read()
-        return 'success'
-    else:
-        return 'failed'
+@app.route('/', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
 
 if __name__ == '__main__':
     from werkzeug.contrib.fixers import ProxyFix
