32f7b21d1fc7a85f8c07e058d369af69723fb2a8
zhjc1124 <qwerty762@163.com>
2017-09-06 21:20:59 +0800

    test

--- post.py
+++ post.py
@@ -1,10 +1,8 @@
 import requests
-from urllib import urlencode
-
 post_url = 'http://123.206.16.144'
 data = {'username': 'qwerty', 'password': 'zxcvbn'}
 
-# data = urlencode(data).encode('utf8')
+
 img = {'file': open('test.jpg', 'rb')}
 response = requests.post(post_url, data=data, files=img)
 
