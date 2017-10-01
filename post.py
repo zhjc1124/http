ef4a7ea4a06b478a80c0cdc55dcb905165255fe8
zhc1124 <qwerty762@163.com>
2017-10-01 18:26:10 +0800

    test post

--- post.py
+++ post.py
@@ -3,7 +3,7 @@ post_url = 'http://123.206.16.144'
 data = {'username': 'qwerty', 'password': '00000000'}
 
 
-img = {'file': open('test.jpg', 'rb')}
+img = {'file': open('test.png', 'rb')}
 response = requests.post(post_url, data=data, files=img)
 
 print(response.text)
