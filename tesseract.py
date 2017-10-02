eb052b9f131c4a88beee5567050958ad2c4edc5e
zhc1124 <qwerty762@163.com>
2017-10-02 21:16:43 +0800

    pil

--- tesseract.py
+++ tesseract.py
@@ -11,7 +11,7 @@ def to_string(filename):
         lang = 'chi_sim'
         config = '-psm 10 -c tessedit_char_whitelist="辽吉黑冀晋陕鲁皖苏浙豫鄂湘赣台闽滇琼川黔粤甘青渝沪津京宁蒙藏新贵港澳"'
     char = image_to_string(image, lang=lang, config=config)
-    print(filename + '.jpg', char)
+    print filename + '.jpg', char
     return char
 
 
