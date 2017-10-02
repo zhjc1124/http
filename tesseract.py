caee7985b75b8c928a171d01451547264f1d40b6
zhc1124 <qwerty762@163.com>
2017-10-02 21:14:54 +0800

    pil

--- tesseract.py
+++ tesseract.py
@@ -1,3 +1,4 @@
+# -*- coding: utf8-*-
 from pytesseract import image_to_string
 from PIL import Image
 
@@ -5,10 +6,12 @@ from PIL import Image
 def to_string(filename):
     image = Image.open(filename + '.jpg')
     lang = 'eng'
+    config = '-psm 10 -c tessedit_char_whitelist="ABCDEFGHJKLMNPQRSTUVWXYZ0123456789"'
     if filename == '1':
         lang = 'chi_sim'
-    char = image_to_string(image, lang=lang, config='-psm 8')
-    print(char)
-    return image_to_string(image, lang=lang, config='-psm 8')
+        config = '-psm 10 -c tessedit_char_whitelist="辽吉黑冀晋陕鲁皖苏浙豫鄂湘赣台闽滇琼川黔粤甘青渝沪津京宁蒙藏新贵港澳"'
+    char = image_to_string(image, lang=lang, config=config)
+    print(filename + '.jpg', char)
+    return char
 
 
