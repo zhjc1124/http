b0a0d247a2acc4b48a5071e3e7a0a4019318cf46
zhc1124 <qwerty762@163.com>
2017-10-08 12:03:53 +0800

    sql

--- sql.py
+++ sql.py
@@ -3,7 +3,7 @@ import pymysql.cursors
 connection = pymysql.connect(host='127.0.0.1',
                              port=3306,
                              user='root',
-                             password='qwerty7620',
+                             password='zhang19981124',
                              db='http',
                              charset='utf8')
 with connection.cursor() as cursor:
