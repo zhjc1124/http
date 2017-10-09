64e7dc63b2a222e8676c71ea7f371f0e9d9d867a
zhjc1124 <zhjc1124@gmail.com>
2017-10-09 22:30:03 +0800

    sql fix

--- post.py
+++ post.py
@@ -1,9 +1,10 @@
 # -*- coding: utf8-*-
 import requests
+from urllib import urlencode
 post_url = 'http://123.206.16.144'
 data = {'Username': 'qwerty', '\xac\xa6': '00000000', '\xcf\xa6': '120', '0@': u'测试'}
-
-
+data = urlencode(data)
+print type(data)
 img = {'file': open('test.jpg', 'rb')}
 response = requests.post(post_url, data=data, files=img)
 
